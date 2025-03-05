8695dd1e40d944beab424fff96365ea802e2dd91
Subhashree-Sharma <sharmasubhashree88@gmail.com>
2025-03-05 12:11:16 +0530

    [Subhashree Sharma]add.UC_02RegexEmailValidation

--- Regex_EmailValidation/EmailValidation.js
+++ Regex_EmailValidation/EmailValidation.js
@@ -1,11 +1,22 @@
+//UC-02 - RegexEmailValidation(@ is mandatory & valid domain part)
+function validateEmailUC2(email) {
+  let emailRegex = /^abc@bridgelabz\.[a-zA-Z]+$/;
+  return emailRegex.test(email);
+}
+
+//Test cases
+console.log(validateEmailUC2("abc@bridgelabz.co"));        
+console.log(validateEmailUC2("abc@xyz.com"));               
+console.log(validateEmailUC2("abc@bridgelabz"));     
+console.log("\n");    
+
+//UC-01 - RegexEmailValidation(initial regex)
 function validateEmail(email) {
   let emailRegex = /^abc@[a-zA-Z]+\.[a-zA-Z]+$/;
   return emailRegex.test(email);
 }
 
-//Test cases
-console.log(validateEmail("abc@bridgelabz.co"));       
-console.log(validateEmail("abc@bridgelabz.com"));      
-console.log(validateEmail("xyz@bridgelabz.com"));      
-console.log(validateEmail("abc.xyz@bridgelabz.com")); 
+//Test cases           
+console.log(validateEmail("xyz@bridgelabz.com"));    
+console.log(validateEmail("abc@bridgelabz.co"));    
 console.log(validateEmail("abc@.com"));              
