8f04aefb88fc7190ba99e98959594a7809b708ff
Subhashree-Sharma <sharmasubhashree88@gmail.com>
2025-03-05 17:09:55 +0530

    [Subhashree Sharma]add.UC_02 RegexPINCode validation

--- Regex_PINCodeValidation/PinCodeValidation.js
+++ Regex_PINCodeValidation/PinCodeValidation.js
@@ -1,3 +1,15 @@
+//UC-02 Starts with digit 1-9
+function validatePinCode(pin) {
+  let pinRegex = /^[1-9][0-9]{5}$/;
+  return pinRegex.test(pin);
+}
+
+// Test cases
+console.log(validatePinCode("400088")); 
+console.log(validatePinCode("A400088")); 
+console.log(validatePinCode("12345"));   
+
+
 //UC-01 Regex pattern to validate a 6-digit Indian PIN code
 function validatePinCode(pin) {
   let pinRegex = /^[1-9][0-9]{5}$/;
@@ -6,6 +18,6 @@ function validatePinCode(pin) {
 
 // Test cases
 console.log(validatePinCode("400088"));
-console.log(validatePinCode("000088"));  
+console.log(validatePinCode("000088")); 
 console.log(validatePinCode("40-088"));  
-console.log("\n")
+console.log("\n");
